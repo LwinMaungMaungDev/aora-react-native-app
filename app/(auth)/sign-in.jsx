c7c8b23783b57a7105f96a283a8d671341cb5d50
Lwin <lwindev97@gmail.com>
2024-05-05 15:34:59 +0630

    feat[screen]: implement sign-up screen

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -28,8 +28,8 @@ const SignIn = () => {
             resizeMode="contain"
             className="w-[115px] h-[35px]"
           />
-          <Text className="text-2xl text-white font-semibold font-psemibold">
-            Sign up
+          <Text className="text-2xl text-white font-semibold mt-10 font-psemibold">
+            Log in to Aora
           </Text>
           <FormField
             title="Email"
