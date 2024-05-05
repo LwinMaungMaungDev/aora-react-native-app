9f11f5f1ebe2dfdeb08d06259b55ca05c80d43dc
Lwin <lwindev97@gmail.com>
2024-05-05 14:18:28 +0630

    feat: implement [layout] for new route, sign-in and sign-up screens

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,25 @@
-import { View, Text } from "react-native";
+import { View, ScrollView, Image, Text } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { images } from "../../constants";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center h-full px-4 my-6">
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[35px]"
+          />
+          <Text className="text-2xl text-white font-semibold font-psemibold">
+            Sign up
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
