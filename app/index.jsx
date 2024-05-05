af0e2a8a9ca2b1c40f68642557bc6355459b787b
Lwin <lwindev97@gmail.com>
2024-05-05 11:51:25 +0630

    feat: use [router.push] to navigate to sign-in screen. fix: [ScrollView] cannot scroll bug.

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { ScrollView, Image, Text, View } from "react-native";
-import { Link } from "expo-router";
+import { Redirect, router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../constants";
 import CustomButton from "../components/CustomButton";
@@ -8,8 +8,8 @@ import CustomButton from "../components/CustomButton";
 export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
-      <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full justify-center items-center h-full px-4">
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View className="w-full justify-center items-center h-full px-4 mb-20">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -37,7 +37,7 @@ export default function App() {
           </Text>
           <CustomButton
             title="Continue with Email"
-            handlePress={() => {}}
+            handlePress={() => router.push("/sign-in")}
             containerStyles="w-full mt-7"
           />
         </View>
