73e86e6f7d897718c39920bc9751ba3ac33feadf
Lwin <lwindev97@gmail.com>
2024-05-12 18:07:13 +0630

    feat: use fetched videos data to display thumbnails

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -15,7 +15,8 @@ import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
 import EmptyState from "../../components/EmptyState";
 import { getAllPosts } from "../../lib/appwrite";
-import useAppwrite from "../../lib/useAppWrite";
+import useAppwrite from "../../lib/useAppwrite";
+import VideoCard from "../../components/VideoCard";
 
 const Home = () => {
   const { data: posts, refetch } = useAppwrite(getAllPosts);
@@ -28,15 +29,15 @@ const Home = () => {
     setRefreshing(false);
   };
 
+  console.log(posts);
+
   return (
     <SafeAreaView className="bg-primary">
       <FlatList
-        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        data={posts}
         // data={[]}
         keyExtractor={(item) => item.$id}
-        renderItem={({ item }) => (
-          <Text className="text-3xl text-white">{item.id}</Text>
-        )}
+        renderItem={({ item }) => <VideoCard video={item} />}
         ListHeaderComponent={() => (
           <View className="my-6 px-4 space-y-6">
             <View className="justify-between items-start flex-row mb-6">
